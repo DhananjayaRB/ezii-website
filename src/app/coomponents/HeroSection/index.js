b99e490316c3e568fc2af75bf8d35b691fe2c3d1
Dhananjaya <43692598+DhananjayaRB@users.noreply.github.com>
2025-08-01 18:47:46 +0530

    Update index.js

--- src/app/coomponents/HeroSection/index.js
+++ src/app/coomponents/HeroSection/index.js
@@ -8,7 +8,7 @@ import TestimonialsSection from '../TestimonialsSection';
 import ContactForm from '../ContactForm';
 import FAQSection from '../FAQSection';
 import Footer from '../Footer';
-
+import {BlogPost} from '../articles/test';
 const clientLogos = [
   'client-2.webp',
   'client-3.webp',
@@ -184,6 +184,7 @@ export default function HeroSection() {
       
       {/* Footer */}
       <Footer />
+      {/* <BlogPost /> */}
 
       
     </>
